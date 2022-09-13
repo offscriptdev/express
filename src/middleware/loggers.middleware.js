e32bddbb449320f06f553192bc517193cc3841d2
Andrew Craig <roo@offscript.dev>
2022-09-12 20:18:21 -0600

    modularized middleware

--- src/middleware/loggers.middleware.js
+++ src/middleware/loggers.middleware.js
@@ -1,9 +1,17 @@
 // optional logging functions
-function headers(req, res, next) {
-    console.log('ORIGIN: \n' + req.headers.origin);
+const headers = (req, res, next) => {
     console.log('HEADERS: \n' + JSON.stringify(req.headers, null, 2));
+    next();
+}
+
+const origin = (req, res, next) => {
+    console.log('ORIGIN: \n' + req.headers.origin);
+    next();
+}
+
+const body = (req, res, next) => {
     console.log('BODY: \n' + JSON.stringify(req.body, null, 2));
     next();
 }
 
-export { headers }
+export { headers, origin, body }
