438b5195132a275c4d2936f55b3d83b16fe55762
Andrew Craig <roo@offscript.dev>
2022-09-12 20:18:00 -0600

    refactored and added new middleware to routes

--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import * as log from "../middleware/loggers.middleware.js"
-import {send404, sendJson} from "../controllers/web.controllers.js";
+import {send404, helloWorld} from "../controllers/web.controllers.js";
 
 const router = express.Router();
 
-router.get('/', log.headers, sendJson)
+router.get('/', helloWorld)
 
-router.use(log.headers, send404)
+router.use(log.origin, log.headers, log.body, send404)
 
 export default router;
